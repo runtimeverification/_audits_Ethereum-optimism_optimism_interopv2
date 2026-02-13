2ce4b4d61c3f15af7e93527cb4b74d95f5539d5a
geoknee <georgeknee@googlemail.com>
2026-02-13 15:47:47 +0000

    Refactor Finalized Head Management in EngineController
    This commit updates the engine controller to introduce a more flexible
    finalized head management approach. Key changes include:
    
    - Introduce `FinalizedHead()` method to dynamically select finalized
      head
    - Deprecate direct `finalizedHead` field in favor of new method
    - Add support

--- op-node/rollup/iface.go
+++ op-node/rollup/iface.go
@@ -15,6 +15,9 @@ type SuperAuthority interface {
 	// Note that the returned block ref may not be local safe and the caller should
 	// verify the block's local safety before using it.
 	FullyVerifiedL2Head() eth.BlockID
+	// FinalizedL2Head returns the finalized L2 head block reference.
+	// It returns an empty L2BlockRef if no finalized head can be determined.
+	FinalizedL2Head() eth.BlockID
 	// IsDenied checks if a payload hash is denied at the given block number.
 	// Returns true if the payload should not be applied.
 	// The error indicates if the check could not be performed (should be logged but not fatal).
--- op-supernode/supernode/chain_container/super_authority.go
+++ op-supernode/supernode/chain_container/super_authority.go
@@ -30,6 +30,11 @@ func (c *simpleChainContainer) FullyVerifiedL2Head() eth.BlockID {
 	return oldestVerifiedBlock
 }
 
+func (c *simpleChainContainer) FinalizedL2Head() eth.BlockID {
+	// TODO
+	return eth.BlockID{}
+}
+
 // IsDenied checks if a block hash is on the deny list at the given height.
 func (c *simpleChainContainer) IsDenied(height uint64, payloadHash common.Hash) (bool, error) {
 	if c.denyList == nil {
